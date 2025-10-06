46eee38d19e5bc27caf6b573280458f0be695cc0
tidiane <tidianeb240@gmail.com>
2025-10-06 15:01:43 +0100

    edit

---
+++ 01.Drawing/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0,0,225);
+}
+
+function draw() {
+  background(220);
+  circle(200, 250, 50);
+  fill(255,0,0);
+  circle(70,50,50);
+  fill(0,255,0);
+  circle(70,50,50);
+  fill(0,0,255);
+  circle(80,50,50);
+    ellipse(50,200,80,40);
+
+
+    stroke(20,200,200);
+  strokeWeight(18);
+  fill(200,20,200)
+
+  rect(200,200,100,5,50);
+}
+
